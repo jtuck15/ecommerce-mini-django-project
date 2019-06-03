674e99c9039823a7a0fec2a7e312179f7970cf7b
Jimmy Tuck <jim.tuck15@gmail.com>
2019-06-03 19:49:55 +0000

    changing tests.py

--- products/tests.py
+++ products/tests.py
@@ -9,5 +9,5 @@ class ProductTests(TestCase):
     """
 
     def test_str(self):
-        test_name = Product(name='A product')
-        self.assertEqual(str(test_name), 'A product')
+        test_name = Product(name='Product object')
+        self.assertEqual(str(test_name), 'Product object')
